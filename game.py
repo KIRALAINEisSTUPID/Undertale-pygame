2897f2f5c73cf5a2a69207b5fbe59193cbccb9b6
KIRALAINEisSTUPID <k9034449@gmail.com>
2024-11-13 15:44:38 +0500

    idk

--- game.py
+++ game.py
@@ -1,31 +1,24 @@
 import pygame
 import sys
 
-# Инициализация Pygame
 pygame.init()
 
-# Задаем размеры окна
 width, height = 1450, 900
 screen = pygame.display.set_mode((width, height))
 logo = pygame.image.load("Assets/img/icons/soul.png")
 bg_image = pygame.image.load("Assets/img/backgrounds/undertale-game.jpg")
 pygame.display.set_icon(logo)
-# Задаем заголовок окна
 pygame.display.set_caption("Undertale")
 
-# Основной цикл
 running = True
 while running:
-    # Обработка событий
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.blit(bg_image, (0, 0))
 
-    # Обновляем экран
     pygame.display.flip()
 
-# Завершение работы Pygame
 pygame.quit()
 sys.exit()
